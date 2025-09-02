e951b78c7b022997b14528a1a697953bdf93f1f2
Mayukh Das <dasmayukh2019@gmail.com>
2025-09-02 22:07:13 +0530

    Revert "Remove Vercel serverless function handler and update build configurations for Hydrogen app"
    This reverts commit 68467de0a4d42a44a210fe07e91a074730fbfcce.

--- vite.config.js
+++ vite.config.js
@@ -10,13 +10,41 @@ export default defineConfig({
     // Allow a strict Content-Security-Policy
     // without inlining assets as base64:
     assetsInlineLimit: 0,
-    // Optimize for static deployment
-    outDir: 'dist',
+    // Optimize for Vercel deployment
+    target: 'node18',
     rollupOptions: {
-      input: {
-        main: './index.html'
-      }
+      external: [
+        // Add Node.js built-ins that should be external
+        'crypto',
+        'stream',
+        'util',
+        'buffer',
+        'process'
+      ]
     }
+  },
+  ssr: {
+    // Configure for Vercel Edge Runtime compatibility
+    external: [
+      'crypto',
+      'stream', 
+      'util',
+      'buffer',
+      'process'
+    ],
+    optimizeDeps: {
+      /**
+       * Include dependencies here if they throw CJS<>ESM errors.
+       * For example, for the following error:
+       *
+       * > ReferenceError: module is not defined
+       * >   at /Users/.../node_modules/example-dep/index.js:1:1
+       *
+       * Include 'example-dep' in the array below.
+       * @see https://vitejs.dev/config/dep-optimization-options
+       */
+      include: [],
+    },
   },
   // Define global variables for browser compatibility
   define: {
